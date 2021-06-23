5f9362141977c06f50a9a6af64ca66a5a6268444
syed2008 <78297824+syed2008@users.noreply.github.com>
2021-06-23 14:34:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,13 +18,32 @@ speak_data = "taking your selfie in 5 seconds";
 var utterThis = new SpeechSynthesisUtterance(speak_data);
 synth.speak(utterThis);
 Webcam.attach(camera);
+setTimeout(function(){
+    take_snapshot();
+    save();
+
+}, 5000);
 }
+camera = document.getElementById("camera");
 Webcam.set({
     width: 360,
     height: 250,
     image_format: 'png',
   png_quality: 90
 });
-camera = document.getElementById("camera");
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+
+    });
+}
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+    
+}
+
 
 
