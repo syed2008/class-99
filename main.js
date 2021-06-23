3a918170d3911961022c18ae318e17ba338fce4d
syed2008 <78297824+syed2008@users.noreply.github.com>
2021-06-23 14:45:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -31,18 +31,13 @@ Webcam.set({
     image_format: 'png',
   png_quality: 90
 });
-function take_snapshot(){
-    Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
-
-    });
-}
+function take_snapshot() { Webcam.snap(function(data_uri) { document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>'; }); }
 function save(){
     link = document.getElementById("link");
     image = document.getElementById("selfie_image").src;
     link.href = image;
     link.click();
-    
+
 }
 
 
